ccf5e113cd724d5b619dac7e3ea2b714e90b995f
Eito Takase <eito01178@gmail.com>
2023-05-23 21:57:46 +0900

    isr

---
+++ src/pages/[id]/isr.tsx
@@ -0,0 +1,34 @@
+import Head from "next/head";
+
+export const getStaticPaths = () => {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const id = params.id;
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  const json = await res.json();
+  console.log(json);
+
+  return {
+    props: {
+      value: json.title,
+    },
+    revalidate: 10,
+  };
+};
+
+export default function Home({ value }: { value: string }) {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+      <div>getStaticPropsOnly</div>
+      <div>{value}</div>
+    </>
+  );
+}
